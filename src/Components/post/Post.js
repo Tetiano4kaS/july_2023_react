3d69845edae017ad8d9352a09ff1516913b1a007
Tetiano4kaS <stecko332@gmail.com>
2023-07-09 16:54:05 +0300

    hw_2

--- src/Components/post/Post.js
+++ src/Components/post/Post.js
@@ -3,15 +3,13 @@ import PostDetails from "../postDetail/PostDetails";
 
 const Post = (props) => {
     const {post} = props;
-    const [isShow,setIsShow]=useState(false);
+    const [isShow, setIsShow]=useState(false)
     return (
         <div className={'postDiv'}>
             <h4>{post.id}</h4>
             <h3>{post.title}</h3>
-            <button onClick={() => setIsShow(prev => !prev)}>{isShow ? 'Hide' : 'Show'}</button>
-            {
-                isShow && <PostDetails post={post}/>
-            }
+     <button onClick={()=>setIsShow(prev=>!prev)}>{isShow? 'Hide':'Show'}</button>
+            {isShow && <PostDetails post={post}/>}
         </div>
     );
 };
